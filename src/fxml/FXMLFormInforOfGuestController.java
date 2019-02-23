78761d71a364a73c97a0c9c954599002a08c9fb7
an040702 <lendran@gmail.com>
2019-02-24 00:11:57 +0700

    Finish booking

--- src/fxml/FXMLFormInforOfGuestController.java
+++ src/fxml/FXMLFormInforOfGuestController.java
@@ -8,7 +8,14 @@ package fxml;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import static java.lang.Integer.parseInt;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,6 +24,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import models.DAOFormInfo;
+import models.formatCalender;
+import models.FormInfo;
 
 /**
  * FXML Controller class
@@ -25,16 +36,34 @@ import javafx.scene.control.Label;
  */
 public class FXMLFormInforOfGuestController implements Initializable {
 
+    //define
+    
+
+    ObservableList<String> listComboBoxRoom = FXCollections.observableArrayList("Standard", "Superior", "Deluxe", "Suite");
     @FXML
     private Label labelName;
     @FXML
+    private JFXTextField txtFirstName;
+    @FXML
+    private JFXTextField txtLastName;
+    @FXML
+    private JFXTextField txtEmail;
+    @FXML
+    private JFXTextField txtPhoneNumber;
+    @FXML
+    private JFXTextField txtNote;
+    @FXML
+    private JFXTextField txtCompanyName;
+    @FXML
     private JFXComboBox<String> comboBoxRoom;
-
-    ObservableList<String> listComboBoxRoom = FXCollections.observableArrayList("Standard", "Superior", "Deluxe", "Suite");
-    
+    @FXML
+    private JFXTextField txtNumberofGuest;
+    @FXML
+    private JFXDatePicker datePickerDateArrive;
     @FXML
     private JFXCheckBox check1;
-
+    @FXML
+    private JFXTextField txtFlightNumber;
     @FXML
     private JFXButton btnSubmit;
 
@@ -48,10 +77,39 @@ public class FXMLFormInforOfGuestController implements Initializable {
         comboBoxRoom.setValue("Room");
 
         check1.setSelected(true);
+//        if(bookFirstName.getText().isEmpty() && bookLastName.getText().isEmpty() && bookEmail.getText().isEmpty() && bookPhone.getText().isEmpty() && bookNote.getText().isEmpty() && comboBoxRoom.getValue().isEmpty() && bookDate.getValue().isEqual(local) && bookNumber.getText().isEmpty()){
+//            btnSubmit.setDisable(true);
+//        }
+    }
+    Date local;
+
+    @FXML
+    private void handleSubmitAction(ActionEvent event) throws ClassNotFoundException, SQLException, IOException, Exception {
+//        local = LocalDate.now();
+//        if(bookFirstName.getText().isEmpty() && bookLastName.getText().isEmpty() && bookEmail.getText().isEmpty() && bookPhone.getText().isEmpty() && bookNote.getText().isEmpty() && comboBoxRoom.getValue().isEmpty() && bookDate.getValue().isEqual(local) && bookNumber.getText().isEmpty()){
+//            System.out.println("Empty");
+//        }
+        FormInfo Form = new FormInfo();
+        boolean check = false;
+        if (check1.isSelected()) {
+            check = true;
+        }
+        System.out.println(check);
+        System.out.println(txtFirstName.getText());
+        System.out.println(txtLastName.getText());
+        System.out.println(txtEmail.getText());
+        System.out.println(txtPhoneNumber.getText());
+        System.out.println(txtNote.getText());
+        System.out.println(txtCompanyName.getText());
+        System.out.println(comboBoxRoom.getValue());
+        System.out.println(txtNumberofGuest.getText());
+        System.out.println(datePickerDateArrive.getValue());
+        System.out.println(txtFlightNumber.getText());
+        Form.AddNewBooking(txtFirstName.getText(), txtLastName.getText(), txtEmail.getText(), txtPhoneNumber.getText(), txtNote.getText(), txtCompanyName.getText(), comboBoxRoom.getValue(), parseInt(txtNumberofGuest.getText()), datePickerDateArrive.getValue(), check, txtFlightNumber.getText());
     }
 
     @FXML
-    private void handleSubmitAction(ActionEvent event) {
+    private void handleSubmitAction(MouseEvent event) {
     }
 
 }
---
+++ src/models/formatCalender.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javafx.util.StringConverter;
+import javafx.scene.control.DatePicker;
+
+/**
+ *
+ * @author Admin
+ */
+public class formatCalender {
+    public static void format(String pattern,DatePicker idNgayGiao){
+        idNgayGiao.setPromptText(pattern.toLowerCase());
+        idNgayGiao.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(pattern);
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        });
+        //cai nay ha anh, code ni anh kien xoa r :v
+        
+    }
+}
+
