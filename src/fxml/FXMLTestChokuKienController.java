e7d8a15cf8d9a9d5aa9b3474eafe4437f38e929a
Doan Thanh Nhan <doanthanhnhan@outlook.com>
2019-02-26 23:52:36 +0700

    Add test loading scene

--- src/fxml/FXMLTestChokuKienController.java
+++ src/fxml/FXMLTestChokuKienController.java
@@ -5,8 +5,11 @@
  */
 package fxml;
 
+import static controllers.FXMLAccountController.stage;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
@@ -14,8 +17,17 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -42,45 +54,86 @@ public class FXMLTestChokuKienController implements Initializable {
         functiona();
     }
 
+    /**
+     * Để anh cố gắng tạo 1 class chạy loading chung để lấy ra dùng nhiều lần cho nó dễ
+     * Đã có loading đẹp, nhưng chưa hiểu code nên chưa ném vào đây ^_^
+     */
     public void functiona() {
         System.out.println(mainPane.lookup("#btn_test"));
 
         Button btn = (Button) mainPane.lookup("#btn_test");
         btn.setDisable(true);
-//        for (int i = 0; i < 100000; i++) {
-//            System.out.println(i);
-//        }
-        //btnTest.setDisable(true);
-        //btnTest.setDisable(false);
 
-        Task loadOverview = new Task() {
-            @Override
-            protected Object call() throws Exception {
-                //System.out.println("Loading...");
-                for (int i = 0; i < 100000; i++) {
-                    System.out.println("Loading..."+ i);
-                }
-                return null;
-            }
-        };
+        // Đoạn này làm loading (đang làm chạy vô tận)
         
-        loadOverview.setOnSucceeded(new EventHandler<Event>() {
-            @Override
-            public void handle(Event event) {
-                System.out.println("Finished");
-                Platform.runLater(() -> {
-                    
-                    btn.setDisable(false);
-
-                });
-            }
+        // Khai báo stage nhìn xuyên thấu
+        final Stage stage = new Stage(StageStyle.TRANSPARENT);
+        
+        // Chỗ này set khi mở cửa sổ con lên thì cha bị vô hiệu
+        stage.initModality(Modality.APPLICATION_MODAL);
+        
+        final Label status = new Label("Loading");
+        final ProgressIndicator indicator = new ProgressIndicator(ProgressIndicator.INDETERMINATE_PROGRESS);
+        final Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(1), new EventHandler() {
+                    @Override
+                    public void handle(Event event) {
+                        String statusText = status.getText();
+                        status.setText(
+                                ("Loading . . .".equals(statusText))
+                                ? "Loading ."
+                                : statusText + " ."
+                        );
+                    }
+                }),
+                new KeyFrame(Duration.millis(1000))
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        Platform.runLater(() -> {
+            VBox layout = new VBox();
+            layout.setAlignment(Pos.CENTER);
+            layout.setSpacing(10);
+            layout.getChildren().addAll(indicator, status);
+            layout.setStyle("-fx-padding: 10;");
+            stage.setScene(new Scene(layout, 100, 100));
+            stage.show();
         });
 
-        new Thread(loadOverview).start();
-//        Platform.runLater(() -> {
-//            btn.setDisable(false);
+        timeline.play();
+        //Hết đoạn loading
+        
+        //Muốn làm loading dừng lại thì phải chạy 1 luồng khác tắt cái timelime.stop() -- Xem bên main form anh làm.
+        //Khuya rồi nên làm demo vầy cho em xem rồi tính tiếp.
+        //Cách dưới là 1 nhiệm vụ chạy 1 luồng, em muốn nhiều nhiệm vụ chạy cùng 1 lúc thì tạo nhiều task, và new Thread(Tên task).start()
+        //để chạy luồng mới.
+        //Chạy đa luồng yêu cầu em phải kiểm soát (tức là đoán được code của mình nó chạy như thế nào để debug)
+        
+        // Đoạn này làm đa luồng.
+//        Task loadOverview = new Task() {
+//            @Override
+//            protected Object call() throws Exception {
+//                System.out.println("Loading...");
+//                for (int i = 0; i < 100000; i++) {
+//                    System.out.println("Loading..." + i);
+//                }
+//                return null;
+//            }
+//        };
 //
+//        loadOverview.setOnSucceeded(new EventHandler<Event>() {
+//            @Override
+//            public void handle(Event event) {
+//                System.out.println("Finished");
+//                Platform.runLater(() -> {
+//
+//                    btn.setDisable(false);
+//
+//                });
+//            }
 //        });
+//
+//        new Thread(loadOverview).start();
+  
     }
 
 }
