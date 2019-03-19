c9499a5556d08a9f35275cce3fa1186ec9b687c0
karobest3 <karobest3@gmail.com>
2019-03-19 15:10:03 +0700

    's'

--- src/test/FXMLAddNewRoomLoader.java
+++ src/test/FXMLAddNewRoomLoader.java
@@ -1,45 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package test;
-
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-/**
- *
- * @author Doan Thanh Nhan
- */
-public class FXMLAddNewServiceTypeLoader extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        try {
-            System.out.println("Kiểm tra : "+ getClass().getResource("/fxml/FXMLAddNewServiceType.fxml").getPath());
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXMLAddNewServiceType.fxml"));
-
-            Scene scene = new Scene(root);
-
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(FXMLAddNewServiceTypeLoader.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package test;
+//
+//import java.io.IOException;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//
+///**
+// *
+// * @author Doan Thanh Nhan
+// */
+//public class FXMLAddNewServiceTypeLoader extends Application {
+//
+//    @Override
+//    public void start(Stage stage) {
+//        try {
+//            System.out.println("Kiểm tra : "+ getClass().getResource("/fxml/FXMLAddNewServiceType.fxml").getPath());
+//            Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXMLAddNewServiceType.fxml"));
+//
+//            Scene scene = new Scene(root);
+//
+//            stage.setScene(scene);
+//            stage.show();
+//        } catch (IOException ex) {
+//            Logger.getLogger(FXMLAddNewServiceTypeLoader.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//
+//}
